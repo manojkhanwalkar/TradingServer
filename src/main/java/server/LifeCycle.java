da06dc62c96dc8f68eed2e780f2eeab26ff75160
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2014-11-18 23:47:37 -0500

    Initial Maven Structure

--- src/main/java/server/LifeCycle.java
+++ src/main/java/server/LifeCycle.java
@@ -1,8 +1,20 @@
 package server;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by mahesh on 11/18/14.
  */
 public class LifeCycle
 {
+
+    public static void main(String[] args)
+    {
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/application-context.xml");
+
+    }
+
+
 }
